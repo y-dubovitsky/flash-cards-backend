0da4e97a0203da56b05c3650d03679d4f056778a
User <y.dubovitsky@gmail.com>
2020-12-18 12:59:44 +0300

    Init commit.

---
+++ src/test/java/space/dubovitsky/flashcards/FlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package space.dubovitsky.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
